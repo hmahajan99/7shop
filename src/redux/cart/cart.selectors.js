9c9cd4bf4cbb3580f68878ce2465f93cec665ebf
Harshit Mahajan <hmahajan1999@gmail.com>
2019-08-15 18:41:35 +0530

    added checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -3,15 +3,31 @@ import { createSelector } from 'reselect';
 const selectCart = state => state.cart;
 
 export const selectCartItems = createSelector(
-    [selectCart],
-    (cart) => cart.cartItems 
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
 );
 
 export const selectCartItemsCount = createSelector(
-    [selectCartItems],
-    cartItems => 
-        cartItems.reduce(
-            (accumulatedQuantity, cartItem) => accumulatedQuantity+cartItem.quantity,
-            0
-        )
-)
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity * cartItem.price,
+      0
+    )
+);

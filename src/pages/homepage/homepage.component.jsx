939e532bd0a6f26fc3f8ed0fe9424e5824b8b370
Harshit Mahajan <hmahajan1999@gmail.com>
2019-08-27 20:44:41 +0530

    converted homepage,header,custom button to styled component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Directory from '../../components/directory/directory.component'
+import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss'
+import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => (
-    <div className='homepage'>
-        <Directory />
-    </div>
-)
+  <HomePageContainer>
+    <Directory />
+  </HomePageContainer>
+);
 
 export default HomePage;

288361a26f2ab08f84f18b45a508eaafbe2c7a16
Harshit Mahajan <hmahajan1999@gmail.com>
2019-08-30 22:28:11 +0530

    added spinner hoc

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss'
 
-const CollectionPreview = ({ title, items }) => (
+const CollectionPreview = ({ title, items, history, match, routeName }) => (
     <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
+        <h1 className='title' onClick={() => history.push(`${match.path}/${routeName}`)}>
+            {title.toUpperCase()}
+        </h1>
         <div className='preview'>
             {
                 items
@@ -19,4 +22,4 @@ const CollectionPreview = ({ title, items }) => (
     </div>
 )
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);

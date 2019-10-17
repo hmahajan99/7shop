89739d3ef8aaf3e1b10429c47f4613dfdfe6001a
Harshit Mahajan <hmahajan1999@gmail.com>
2019-10-17 21:17:23 +0530

    updated code to be pwa compliant

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
@@ -21,3 +22,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+serviceWorker.register();
